b5058c07174d5434b7682c69873dbceed1c58d5d
Muhammad Aldo Saputra <maldosptra563@gmail.com>
2025-08-31 21:40:30 +0700

    bug fix

--- app/Http/Controllers/Index/DashboardController.php
+++ app/Http/Controllers/Index/DashboardController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\Index;
 
 use App\Http\Controllers\Controller;
+use App\Models\JadwalPengampu;
+use App\Models\Kegiatan;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('dashboard.index');
+        $user = Role::withCount('users')->get();
+
+        $jadwal = JadwalPengampu::select('hari', DB::raw('count(*) as total'))->groupBy('hari')->get();
+        return view('dashboard.index', compact('user', 'jadwal'));
     }
 }
